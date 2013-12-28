1571b289860ad90427884d3972fd9cea098db0fd
Sc2slash <pedro.m94@hotmail.com>
2013-12-28 19:40:55 +0100

    Random stuff

--- Eldar/src/eldar/game/core/levels/tiles/Tile.java
+++ Eldar/src/eldar/game/core/levels/tiles/Tile.java
@@ -1,5 +1,28 @@
 package eldar.game.core.levels.tiles;
 
-public class Tile {
+import eldar.game.core.graphics.Screen;
+import eldar.game.core.graphics.Sprite;
+import eldar.game.core.graphics.Spritesheet;
 
+public class Tile {
+	
+	
+	private final Spritesheet TILESET = new Spritesheet("/graphics/dirt.jpg");
+	private final int TILE_WIDTH  = 32;
+	private final int TILE_HEIGHT = 32;
+	
+	private int id;
+	private Sprite sprite;
+	
+	public Tile(int id){
+	}
+	public boolean isSolid(){
+		return false;
+	}
+	public void interact(){
+		
+	}
+	public void render(Screen screen, int x, int y){
+		screen.render(x, y, sprite);
+	}
 }
